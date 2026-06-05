c76c80219d59e405b7761e8b3561092a72a3c5e1
Pato Anabalon <pato.anabalon@gmail.com>
2026-06-05 10:07:36 -0400

    chore: update project documentation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  allowedDevOrigins: ['192.168.1.33']
 };
 
 export default nextConfig;
