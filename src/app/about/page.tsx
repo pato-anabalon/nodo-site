e6e2d3f431026f466a626e2e724756756241c944
Pato Anabalon <pato.anabalon@gmail.com>
2026-06-10 18:20:59 -0400

    delete about page from sitemap (SEO)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,20 +1,15 @@
 import type { Metadata } from "next";
-import { JsonLdScript } from "@/components/atoms/JsonLdScript";
 import { RoutePlaceholder } from "@/components/molecules/RoutePlaceholder";
 import { placeholderPages } from "@/lib/content";
-import { createBreadcrumbStructuredData, createPageMetadata } from "@/lib/seo";
+import { createPageMetadata } from "@/lib/seo";
 
 export const metadata: Metadata = createPageMetadata({
   title: "About",
   description: placeholderPages.about.description,
   path: "/about",
+  noIndex: true,
 });
 
 export default function AboutPage() {
-  return (
-    <>
-      <JsonLdScript id="breadcrumb-structured-data" data={createBreadcrumbStructuredData("/about")} />
-      <RoutePlaceholder pageKey="about" headingLevel="h1" {...placeholderPages.about} />
-    </>
-  );
+  return <RoutePlaceholder pageKey="about" headingLevel="h1" {...placeholderPages.about} />;
 }
